be2cc4886f86fd2ff09873a680b65f4c17c9b972
Никита Сизов <sizov.space@gmail.com>
2023-02-12 14:46:55 +0200

    build

--- gulpfile.js
+++ gulpfile.js
@@ -67,15 +67,8 @@ gulp.task('clean', async function() {
 });
 
 gulp.task('img', function() {
-    return gulp.src('app/img/**/*') // Берем все изображения из app
-        .pipe(cache(imagemin({ // С кешированием
-        // .pipe(imagemin({ // Сжимаем изображения без кеширования
-            interlaced: true,
-            progressive: true,
-            svgoPlugins: [{removeViewBox: false}],
-            use: [pngquant()]
-        }))/**/)
-        .pipe(gulp.dest('dist/img')); // Выгружаем на продакшен
+    return gulp.src('app/src/**/*') // Берем все изображения из app
+        .pipe(gulp.dest('dist/src')); // Выгружаем на продакшен
 });
 
 gulp.task('prebuild', async function() {
